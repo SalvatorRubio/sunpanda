2fcf03e1f1851dcdbaa4cd8e837055c4e250a4fa
Matvey <matvejilchenko9388@gmail.com>
2022-10-23 16:31:05 +0300

    Создал проект

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,22 @@
+import Header from "@components/Header";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "../HomePage";
+import CityPage from "../CityPage";
+
+import styles from './App.module.css'
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <main className={styles.container}>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/City-:name" element={<CityPage/>}/>
+                </Routes>
+            </main>
+        </>
+    );
+}
+
+export default App;
